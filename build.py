9c47ba4f746e64259e94a5130827d3d8153c4c62
Evgeni Kunev <evgeni@kunev.me>
2017-10-15 17:03:12 +0300

    Allow building only specific lectures

--- build.py
+++ build.py
@@ -1,20 +1,36 @@
 import os
 import subprocess
 import shutil
+import argparse
+
+import yaml
+
+
+NOTES_COMMAND = ('jupyter nbconvert --to html lectures/{slug}.ipynb --stdout '
+                 '> compiled-lectures/{slug}.html')
+SLIDES_COMMAND = ('jupyter nbconvert --to slides lectures/{slug}.ipynb '
+                  '--reveal-prefix=reveal.js --output-dir=slides ')
+
 
 os.makedirs('compiled-lectures', exist_ok=True)
 os.makedirs('slides', exist_ok=True)
 
-files = [name for name in os.listdir('lectures') if name.endswith('.ipynb')]
-for lecture in files:
-    slug = os.path.splitext(lecture)[0]
-    notes_command = ('jupyter nbconvert --to html lectures/{slug}.ipynb --stdout '
-                     '> compiled-lectures/{slug}.html').format(slug=slug)
-    slides_command = ('jupyter nbconvert --to slides lectures/{slug}.ipynb '
-                      '--reveal-prefix=reveal.js --output-dir=slides ').format(
-                          slug=slug)
-    notes_error_code = subprocess.call(notes_command, shell=True)
-    slides_error_code = subprocess.call(slides_command, shell=True)
+
+parser = argparse.ArgumentParser(description='Compile lectures')
+parser.add_argument('--lectures', metavar='L', type=int, nargs='+',
+                    help='lecture indexes to compile as written in index.yaml',
+                    required=False)
+args = parser.parse_args()
+
+
+INDEX = yaml.load(open('lectures/index.yml'))
+
+
+def build_lecture(slug):
+    notes_error_code = subprocess.call(NOTES_COMMAND.format(slug=slug),
+                                       shell=True)
+    slides_error_code = subprocess.call(SLIDES_COMMAND.format(slug=slug),
+                                        shell=True)
     if notes_error_code:
         raise RuntimeError("Failed to compile the lectures: {}".format(
             notes_error_code))
@@ -23,3 +39,13 @@ for lecture in files:
             slides_error_code))
 
     shutil.copy('lectures/index.yml', 'compiled-lectures/index.yml')
+
+
+if args.lectures:
+    for lecture_index in args.lectures:
+        build_lecture(INDEX[lecture_index]['slug'])
+else:
+    files = [name for name in os.listdir('lectures') if name.endswith('.ipynb')]
+    for lecture in files:
+        slug = os.path.splitext(lecture)[0]
+        build_lecture(slug)
